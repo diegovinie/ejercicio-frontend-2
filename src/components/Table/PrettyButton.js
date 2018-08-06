76f92ec48b636cab5b83d3c9d74c18cc205f8843
Diego Viniegra <diego.viniegra@gmail.com>
2018-08-06 16:40:50 -0500

    fea: Añade callback al click en PrettyButton

--- src/components/Table/PrettyButton.js
+++ src/components/Table/PrettyButton.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 export default function PrettyButton (props) {
+
+  const handleClick = (ev) => {
+    console.log('callback')
+    props.callback(ev)
+  }
+
   return (
     <button
       id="btn-edit"
       type="button"
       className="classButtons"
+      onClick={(ev) => handleClick(ev)}
       >
       {props.children}
     </button>
