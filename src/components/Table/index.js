0bae3da924389c5188128ec7d6f731ea7dba8fdf
Diego Viniegra <diego.viniegra@gmail.com>
2018-08-07 11:37:23 -0500

    doc: Se amplía doc de Table
    Se documentan los nuevos métodos y las variables.

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -36,6 +36,7 @@ export default class Table extends React.Component
     // Llama al constructor de React.Component
     super(props)
 
+    // Se amarran los métodos con la clase
     this.updateFiltered = this.updateFiltered.bind(this)
     this.swapEditable = this.swapEditable.bind(this)
     this.swapCurrency = this.swapCurrency.bind(this)
@@ -49,6 +50,11 @@ export default class Table extends React.Component
     this.editEmployee = this.editEmployee.bind(this)
     this.printEmployees = this.printEmployees.bind(this)
 
+    /**
+     * El molde para crear un nuevo empleado.
+     *
+     * @var {object}
+     */
     this.employePattern = {
       name: '',
       company: '',
@@ -59,28 +65,55 @@ export default class Table extends React.Component
     }
 
     this.state = {
-      employees,           // (array) Lista de los empleados
-      filteredEmployees: employees,
-      editable: false,
-      usd: false,
-      exchange: 21.50,
-      addDialog: false,
-      newEmployee: this.employePattern
+      employees,                        // (array) Lista de los empleados
+      filteredEmployees: employees,     // (array) Lista de empleados filtrados
+      editable: false,                  // (bool) Controla el modo edición
+      usd: false,                       // (bool) Controla la moneda de salarios
+      exchange: 21.50,                  // (float) El tipo de cambio
+      addDialog: false,                 // (bool) Controla el formulario AddEmployee
+      newEmployee: this.employePattern  // (object) Es el molde del nuevo empleado
     }
   }
 
+  /**
+   * Actualiza la lista de empleados filtrados.
+   *
+   * @listens event:onSearchChange
+   *
+   * @param {array} filteredList La lista que llega de SearchBar.
+   * @param {array} state.filteredList La lista filtrada en estado.
+   */
   updateFiltered (filteredList) {
     this.setState({filteredEmployees: filteredList})
   }
 
+  /**
+   * Activa el modo edición.
+   *
+   * @listens PrettyButton#edit~event:callback
+   */
   swapEditable () {
     this.setState((state) => {return {editable: !state.editable}})
   }
 
+  /**
+   * Cambia entre pesos mexicanos y dólares.
+   *
+   * @listens PrettyButton#currency~event:callback
+   */
   swapCurrency () {
     this.setState((state) => {return {usd: !state.usd}})
   }
 
+  /**
+   * Elimina un empleado de la lista normal y filtrada.
+   *
+   * @listens event:onDeleteEmployee
+   *
+   * @param {number} id El id del empleado a eliminar.
+   * @param {array} state.employees La lista de empleados.
+   * @param {array} statte.filteredEmployees La lista de empleados filtrados.
+   */
   deleteEmployee (id) {
     this.setState((state) => {
       let employees = state.employees.filter((item) => item.id !== id)
@@ -90,16 +123,37 @@ export default class Table extends React.Component
     })
   }
 
+  /**
+   * Activa y desactiva el formulario de AddEmployee.
+   *
+   * @listens PrettyButton#add~event:callback
+   */
   swapDialog () {
     this.setState((state) => {return {addDialog: !state.addDialog}})
   }
 
+  /**
+   * Actualiza el nuevo empleado conforme se va escribiendo.
+   *
+   * @listens event:onKeyChange
+   *
+   * @param {object} employee El nuevo empleado que llega del formulario.
+   * @param {object} state.newEmployee Los datos del nuevo empleado en memoria.
+   */
   keyChange (employee) {
     this.setState({newEmployee: employee})
   }
 
+  /**
+   * Guarda un nuevo empleado.
+   *
+   * @listens event:onFormSubmit
+   *
+   * @param {array} state.employees La lista de empleados.
+   * @param {object} state.newEmployee Los campos del nuevo empleado.
+   * @param {array} state.filteredEmployees Los empleados filtrados.
+   */
   storeEmployee () {
-
     this.setState((state) => {
       return {
         employees: state.employees.concat(state.newEmployee)
@@ -111,21 +165,49 @@ export default class Table extends React.Component
     this.swapDialog()
   }
 
+  /**
+   * Reinicia los campos de la variable newEmployee
+   *
+   * @param {obect} state.newEmployee
+   * @param {object} this.employePattern El molde
+   */
   clearNewEmployee () {
     this.setState({newEmployee: this.employePattern})
   }
 
+  /**
+   * Busca el póximo id para agregar un empleado
+   *
+   * @param {array} state.employees La lista de empleados.
+   *
+    @return {number} El próximo id.
+   */
   getNextId () {
     return this.state.employees.reduce((acc, item) => {
         return item.id > acc.id ? item : acc
       }).id + 1
   }
 
+  /**
+   * Cierra AddEmployee y reinicia sus campos
+   *
+   * @listens event:onCancel
+   */
   cancelAndReturn () {
     this.clearNewEmployee()
     this.swapDialog()
   }
 
+  /**
+   * Actualiza el estado cuando se está editando un empleado.
+   *
+   * @listens event:onEditEmployee
+   *
+   * @param {number} id El id del empleado editado.
+   * @param {string} key El Campo del empleado editado.
+   * @param {string} value El nuevo valor.
+   * @param {array} state.employees La lista de empleados.
+   */
   editEmployee (id, key, value) {
     this.setState((state) => {
       let updatedEmployees = state.employees.map((item) => {
@@ -138,6 +220,14 @@ export default class Table extends React.Component
     })
   }
 
+  /**
+   * Imprime el array de empleados en consola.
+   *
+   * @listens PrettyButton#print~event:callback
+   *
+   * @param {array} state.employees Los empleados.
+   * @param {array} state.filteredEmployees Los empleados filtrados.
+   */
   printEmployees () {
     let cloneFiltered = JSON.parse(JSON.stringify(this.state.filteredEmployees))
     let cloneEmployees = JSON.parse(JSON.stringify(this.state.employees))
@@ -146,7 +236,7 @@ export default class Table extends React.Component
   }
 
   render () {
-
+    // Muestra el tipo de cambio
     let currencyTooltip =
     <span>
       Cambiar a  {(this.state.usd ? 'MXN$' : 'USD$')}
